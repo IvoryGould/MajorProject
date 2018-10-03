ab915bed6badcb5efa61b40b52854bdc4ba00154
IvoryGould <ivory.gould@gmail.com>
2018-10-03 16:19:58 +1000

    Movement
    Added Basic Movement

---
+++ MajorProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    float moveSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Movement();
+
+    }
+
+    void Movement() {
+
+        float moveH = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float moveV = Input.GetAxis("Vertical") * Time.deltaTime;
+
+        this.transform.Translate((moveV * moveSpeed), 0, (moveH * moveSpeed));
+
+    }
+}
