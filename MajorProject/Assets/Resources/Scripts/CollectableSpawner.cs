7ee15ea3583c7efc2de7186f364d49b8df76e2b9
IvoryGould <ivory.gould@gmail.com>
2018-11-01 16:55:15 +1100

    Minor Changes
    ...

---
+++ MajorProject/Assets/Resources/Scripts/CollectableSpawner.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableSpawner : MonoBehaviour {
+
+    int collectableIndex;
+
+    public List<GameObject> collectables = new List<GameObject>();
+
+    void Awake()
+    {
+
+        collectables.Add(Resources.Load<GameObject>("Prefabs/blue"));
+        collectables.Add(Resources.Load<GameObject>("Prefabs/red"));
+        collectables.Add(Resources.Load<GameObject>("Prefabs/yellow"));
+
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        collectableIndex = Random.Range(0, 3);
+        CollectableSpawn();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CollectableSpawn()
+    {
+
+        Instantiate(collectables[collectableIndex], new Vector3(this.transform.position.x, 2, this.transform.position.z), Quaternion.identity, this.transform);
+        int lastChildIndex = this.transform.childCount - 1;
+        this.transform.GetChild(lastChildIndex).transform.localScale = new Vector3(0.5f, 1, 0.5f);
+
+    }
+
+    void OnCollisionExit(Collision collision) {
+
+        if (collision.gameObject.tag == "Bullet") {
+
+            Destroy(collision.gameObject);
+            Destroy(this.transform.GetChild(0).gameObject);
+            Instantiate(collectables[collectableIndex], new Vector3(this.transform.position.x, 1, this.transform.position.z), Quaternion.identity);
+
+        }
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "TrainDeleter")
+        {
+
+            DeleteTrain();
+
+        }
+
+    }
+
+    void DeleteTrain() {
+
+        Destroy(this.transform.parent.gameObject);
+
+    }
+}
---
+++ MajorProject/Assets/Resources/Scripts/Train.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Train : MonoBehaviour {
+
+    public float moveSpeed = 5;
+    public float timerNum = 5;
+
+    void Awake() {
+
+    }
+
+    // Use this for initialization
+    void Start () {
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (this.transform.position == new Vector3(0, 1, 30))
+        {
+
+            StartCoroutine(Timer());
+
+        }
+
+    }
+
+    void Movement() {
+
+        this.transform.Translate(0, 0 , (-moveSpeed * Time.deltaTime));
+
+    }
+
+    void DeleteTrain() {
+
+        Destroy(this.gameObject);
+
+    }
+
+    void OnCollisionEnter(Collision collision) {
+
+        if (collision.gameObject.tag == "TrainDeleter") {
+
+            DeleteTrain();
+
+        }
+
+    }
+
+    IEnumerator Timer() {
+
+        yield return new WaitForSeconds(timerNum);
+        Movement();
+
+    }
+
+
+}
